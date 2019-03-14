2261b8aa9e6068feb5d320b2014d03ce5646c616
abdul-majid-ashrafi <dear_majid01@yahoo.com>
2019-03-13 22:19:16 -0700

    user swipeout

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 import { Container, Header, Title, Right, Body } from "native-base";
-
+import Swipeout from 'react-native-swipeout';
 import { connect } from 'react-redux';
 
 
 class ListItemComponent extends Component {
+
+	removeHandler = (item) => {
+		console.log("REMOVE ITEM ", item)
+		// console.log("this.props.todoList ", this.props.todoList[item.index])
+	}
+
+	onOpen(item) {
+		console.log("on Open ", item)
+	}
+
+	onClose = (item) => {
+		console.log("ONclone ", item)
+	}
+
 	render() {
+		var swipeoutBtns = [
+			{
+				text: 'Delete',
+				backgroundColor: '#ff0000',
+				// onPress: (a) => { console.log("XXXXXXXXXXXXX ", a) }
+			}
+		]
+
 		return (
 			<Container>
 				<Header span style={styles.header}>
@@ -19,11 +41,13 @@ class ListItemComponent extends Component {
 					data={this.props.todoList}
 					keyExtractor={(item, index) => index.toString()}
 					renderItem={info => (
-						<TouchableOpacity>
-							<View style={styles.listItem}>
-								<Text>{info.item.value}</Text>
-							</View>
-						</TouchableOpacity>
+						<Swipeout right={swipeoutBtns} onOpen={this.onOpen.bind(this, info)} onClose={this.onClose.bind(this, info)}>
+							<TouchableOpacity>
+								<View style={styles.listItem}>
+									<Text>{info.item.value}</Text>
+								</View>
+							</TouchableOpacity>
+						</Swipeout>
 					)}
 				/>
 			</Container>
